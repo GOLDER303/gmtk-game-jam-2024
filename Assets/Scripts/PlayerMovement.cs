b090644bdc06a28121922a0955c9063143d058f1
GOLDER303 <szymonstepien@interia.pl>
2024-08-16 20:43:11 +0200

    Add basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 6f;
+
+    private Vector2 moveVector;
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = new Vector3(moveVector.x, 0f, moveVector.y).normalized;
+
+        if (direction.magnitude >= .1f)
+        {
+            characterController.Move(speed * Time.deltaTime * direction);
+        }
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        moveVector = inputValue.Get<Vector2>();
+    }
+}
