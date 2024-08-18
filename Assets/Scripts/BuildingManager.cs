47a7e065143d9f8a6698a9b0bb701ab174b71433
GOLDER303 <szymonstepien@interia.pl>
2024-08-18 14:13:20 +0200

    Add UI for building buildings

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public void BuildBuilding(int towerId)
+    {
+        Debug.Log($"Building building with id: {towerId}");
+    }
+}
