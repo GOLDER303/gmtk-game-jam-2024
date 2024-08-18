75cc27f18ba4a003052d7dbf4538fdc22bce4a6c
GOLDER303 <szymonstepien@interia.pl>
2024-08-18 13:06:14 +0200

    Add player movement animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed = 6f;
 
+    [SerializeField]
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private SpriteRenderer playerSprite;
+
     private Vector2 moveVector;
 
     private Rigidbody rb;
@@ -24,6 +30,16 @@ public class PlayerMovement : MonoBehaviour
         if (direction.magnitude >= .1f)
         {
             rb.velocity = speed * direction;
+            playerAnimator.SetFloat("Speed", speed);
+
+            if (direction.x > 0)
+            {
+                playerSprite.flipX = true;
+            }
+            else if (direction.x < 0)
+            {
+                playerSprite.flipX = false;
+            }
         }
     }
 }
