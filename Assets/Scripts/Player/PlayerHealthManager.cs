2deddd0baf14ec814bcf59fc8cf97af5bc883dd0
GOLDER303 <szymonstepien@interia.pl>
2024-08-21 17:53:55 +0200

    Add on hit flash effect to the player

--- Assets/Scripts/Player/PlayerHealthManager.cs
+++ Assets/Scripts/Player/PlayerHealthManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
@@ -11,15 +12,30 @@ public class PlayerHealthManager : MonoBehaviour
     [SerializeField]
     private int maxHealth = 500;
 
+    [SerializeField]
+    private Material hitFlashMaterial;
+
+    [SerializeField]
+    private float flashEffectDuration = .1f;
+
+    private SpriteRenderer playerSpriteRenderer;
+    private Material defaultMaterial;
+    private Coroutine flashEffectCoroutine;
+
     private HealthSystem healthSystem;
 
     private void Start()
     {
+        playerSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        defaultMaterial = playerSpriteRenderer.material;
+
         healthSystem = new(maxHealth, healthBar);
     }
 
     public void DealtDamage(float damageAmount)
     {
+        PlayFlashEffect();
+
         healthSystem.DealDamage(damageAmount);
 
         if (healthSystem.Health <= 0)
@@ -32,5 +48,26 @@ public class PlayerHealthManager : MonoBehaviour
     {
         OnPlayerDeath?.Invoke();
         Debug.Log("game over");
+    }
+
+    private void PlayFlashEffect()
+    {
+        if (flashEffectCoroutine != null)
+        {
+            StopCoroutine(flashEffectCoroutine);
+        }
+
+        flashEffectCoroutine = StartCoroutine(FlashEffectCoroutine());
+    }
+
+    private IEnumerator FlashEffectCoroutine()
+    {
+        playerSpriteRenderer.material = hitFlashMaterial;
+
+        yield return new WaitForSeconds(flashEffectDuration);
+
+        playerSpriteRenderer.material = defaultMaterial;
+
+        flashEffectCoroutine = null;
     }
 }
